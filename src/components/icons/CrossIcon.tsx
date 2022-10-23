7a68f3a8e694a6c041257823f351b8dbc309b7cd
Ryan Conceicao <ryoid@users.noreply.github.com>
2022-10-24 00:36:56 +0800

    fix type icon

--- src/components/icons/CrossIcon.tsx
+++ src/components/icons/CrossIcon.tsx
@@ -1,8 +1,6 @@
-import React, { FC } from "react";
+import React from "react";
 
-type CrossIconProps = {};
-
-const CrossIcon: FC<CrossIconProps> = ({}) => {
+const CrossIcon: React.FC = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"

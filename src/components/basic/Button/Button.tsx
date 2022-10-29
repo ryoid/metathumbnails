91714a1aaae3b751a36675bbb54109d6e18b7f5d
Ryan Conceicao <ryoid@users.noreply.github.com>
2022-10-29 11:57:59 +0800

    feat: button, input sizes

--- src/components/basic/Button/Button.tsx
+++ src/components/basic/Button/Button.tsx
@@ -7,6 +7,8 @@ type PrimitiveButtonProps = React.ComponentPropsWithoutRef<"button">;
 type ButtonProps = PrimitiveButtonProps & {
   color?: Color;
   variant?: "solid" | "outline";
+  size?: "xs" | "sm" | "md";
+  center?: boolean;
 };
 
 const NAME = "Button";
@@ -15,14 +17,26 @@ const Button: React.FC<ButtonProps> = ({
   color = "gray",
   variant = "solid",
   type = "button",
+  size = "md",
+  center,
+  disabled,
   ...props
 }) => {
   return (
     <button
       type={type}
+      disabled={disabled}
       {...props}
       className={cn(
-        "inline-flex select-none items-center gap-2 rounded  border px-4 py-2 font-brand font-medium",
+        "inline-flex select-none items-center rounded border font-brand font-medium",
+        {
+          "justify-center": center,
+        },
+        {
+          "gap-2 px-4 py-2": size === "md",
+          "gap-0.5 px-2.5 py-1 text-sm": size === "sm",
+          "gap-0.5 px-2 py-0.5 text-xs": size === "xs",
+        },
         {
           "text-white": true,
           "border-indigo-500 bg-indigo-600 hover:border-indigo-400 hover:bg-indigo-500":
@@ -32,6 +46,9 @@ const Button: React.FC<ButtonProps> = ({
         },
         {
           "bg-transparent": variant === "outline",
+        },
+        {
+          "cursor-not-allowed text-gray-500 hover:bg-transparent": disabled,
         },
         props.className
       )}
--- src/components/basic/InputText/InputText.tsx
+++ src/components/basic/InputText/InputText.tsx
@@ -3,17 +3,26 @@ import cn from "clsx";
 
 type InputTextElement = React.ElementRef<"input">;
 type PrimitiveInputTextProps = React.ComponentPropsWithoutRef<"input">;
-type InputTextProps = PrimitiveInputTextProps;
+type InputTextProps = PrimitiveInputTextProps & {
+  size?: "xs" | "sm" | "md";
+};
 
 const NAME = "InputText";
 
 const InputText = React.forwardRef<InputTextElement, InputTextProps>(
-  (props, forwardedRef) => {
+  ({ size = "md", ...props }, forwardedRef) => {
     return (
       <input
         {...props}
         className={cn(
-          "group inline-flex h-auto w-full items-center justify-center rounded-lg bg-gray-800/75 py-2.5 pl-4 pr-3.5 text-sm ring-1 ring-inset ring-white/5 [color-scheme:dark] hover:bg-gray-700/40 hover:ring-gray-500 focus:bg-gray-800",
+          "group inline-flex h-auto w-full items-center justify-center rounded-lg bg-gray-800/75 text-sm ring-1 ring-inset ring-white/5 [color-scheme:dark] hover:bg-gray-700/40 hover:ring-gray-500 focus:bg-gray-800",
+          {
+            "py-2.5 pl-4 pr-3.5 ": size === "md",
+            "py-1.5 px-3 text-sm": size === "sm",
+            "px-2 py-0.5 text-xs": size === "xs",
+            // "gap-0.5 px-2.5 py-1 text-sm": size === "sm",
+            // "gap-0.5 px-2 py-0.5 text-xs": size === "xs",
+          },
           props.className
         )}
         ref={forwardedRef}

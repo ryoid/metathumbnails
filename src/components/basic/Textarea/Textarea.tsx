b312e7560f161608461958ff7bcf48ef3427f913
Ryan Conceicao <ryoid@users.noreply.github.com>
2022-11-02 00:42:52 +0800

    fix: textarea style

--- src/components/basic/Textarea/Textarea.tsx
+++ src/components/basic/Textarea/Textarea.tsx
@@ -6,15 +6,16 @@ type PrimitiveTextareaProps = React.ComponentPropsWithoutRef<"textarea">;
 type TextareaProps = PrimitiveTextareaProps;
 
 const NAME = "Textarea";
-const RootCn =
-  "group flex items-center justify-center text-sm h-auto block rounded-lg py-2.5 pl-4 pr-3.5 text-sm ring-1 ring-gray-200 hover:ring-gray-300 bg-gray-800/75 ring-inset ring-white/5 hover:bg-gray-700/40 hover:ring-gray-500 w-full";
 
 const Textarea = React.forwardRef<TextareaElement, TextareaProps>(
   (props, forwardedRef) => {
     return (
       <textarea
         {...props}
-        className={cn(RootCn, props.className)}
+        className={cn(
+          "`h-auto group block w-full items-center justify-center rounded-lg bg-gray-800/75 py-2.5 pl-4 pr-3.5 text-sm ring-1 ring-inset ring-white/5 hover:bg-gray-700/40 hover:ring-gray-500",
+          props.className
+        )}
         ref={forwardedRef}
       />
     );
